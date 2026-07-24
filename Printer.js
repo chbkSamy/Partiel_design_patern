26c96768040f1c670f1c114b9bc525b98d53b706
Samy <chebreksamy@gmail.com>
2026-07-24 16:02:59 +0200

    feat: ajout de Printer avec gestion des paramètres techniques d'impression

---
+++ Printer.js
@@ -0,0 +1,41 @@
+class Printer {
+  constructor() {
+    this.format = null;
+    this.encre = null;
+    this.parametres = {};
+  }
+
+  setFormat(format) {
+    this.format = format;
+  }
+
+  setEncre(encre) {
+    this.encre = encre;
+  }
+
+  setParametres(parametres) {
+    this.parametres = parametres;
+  }
+
+  getFormat() {
+    return this.format;
+  }
+
+  getEncre() {
+    return this.encre;
+  }
+
+  getParametres() {
+    return this.parametres;
+  }
+
+  preparer() {
+    console.log(`Préparation de l'imprimante : format ${this.format}, encre ${this.encre}`);
+  }
+
+  lancer(params) {
+    console.log(`Lancement de l'impression avec les paramètres : ${JSON.stringify(params)}`);
+  }
+}
+
+module.exports = Printer;
